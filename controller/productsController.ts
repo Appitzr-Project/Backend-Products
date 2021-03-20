0ccdff587abf35b643903dfb2e54922d6146b2cc
Irman <irman.herdiana@gmail.com>
2021-03-21 00:28:34 +0700

    Product CRUD - Create/Add

---
+++ controller/productsController.ts
@@ -0,0 +1,205 @@
+import { Response, NextFunction } from "express";
+import { validationResult } from 'express-validator';
+import { products, productsModel } from "@appitzr-project/db-model";
+import { RequestAuthenticated, validateGroup, userDetail } from "@base-pojokan/auth-aws-cognito";
+import * as AWS from 'aws-sdk';
+import { v4 as uuidv4 } from 'uuid';
+
+// declare database dynamodb
+const ddb = new AWS.DynamoDB.DocumentClient({ endpoint: process.env.DYNAMODB_LOCAL, convertEmptyValues: true });
+
+/**
+ * Index Data Function
+ *
+ * @param req
+ * @param res
+ * @param next
+ */
+export const getProductsPublicById = async (
+  req: RequestAuthenticated,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const idProduct = req.param('id');
+
+    // dynamodb parameter
+    const paramDB = {
+      TableName: productsModel.TableName,
+      KeyConditionExpression: "#ip = :idProduct",
+      FilterExpression: "isActive = :ia",
+      ExpressionAttributeNames:{
+          "#ip": "id"
+      },
+      ExpressionAttributeValues: {
+          ":idProduct": idProduct,
+          ":ia": true
+      }
+    }
+
+    // query to database
+    const queryDB = await ddb.query(paramDB).promise();
+
+    // return response
+    return res.json({
+      code: 200,
+      message: "success",
+      data: queryDB?.Items[0]
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
+export const getProductsVenueById = async (
+  req: RequestAuthenticated,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    // validate group
+    const userDetail = await validateGroup(req, "venue");
+
+    const idProduct = req.param('id');
+
+    // dynamodb parameter
+    const paramDB = {
+      TableName: productsModel.TableName,
+      KeyConditionExpression: "#ip = :idProduct AND #vid = :vi",
+      ExpressionAttributeNames:{
+          "#ip": "id",
+          "#vid": "venueId"
+      },
+      ExpressionAttributeValues: {
+          ":idProduct": idProduct,
+          ":vi": userDetail.sub
+      }
+    }
+
+    // query to database
+    const queryDB = await ddb.query(paramDB).promise();
+
+    // return response
+    return res.json({
+      code: 200,
+      message: "success",
+      data: queryDB?.Items[0]
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
+
+/**
+ * Index Data Function
+ *
+ * @param req
+ * @param res
+ * @param next
+ */
+ export const getProductByVenueId = async (
+  req: RequestAuthenticated,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    // validate group
+    const user = await validateGroup(req, "venue");
+    
+    // dynamodb parameter
+    const paramDB = {
+      TableName: productsModel.TableName,
+      IndexName: "venueId-index",
+      KeyConditionExpression: "venueId = :venueId", 
+      ExpressionAttributeValues: {                
+              ":venueId": user.sub              
+          }
+    }
+
+    // query to database
+    const queryDB = await ddb.query(paramDB).promise();
+
+    // return response
+    return res.json({
+      code: 200,
+      message: "success",
+      data: queryDB      
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
+
+/**
+ * Store data to database
+ * 
+ * @param req RequestAuthenticated
+ * @param res Response
+ * @param next NextFunction
+ * @returns 
+ */
+ export const productsStore = async (
+    req: RequestAuthenticated,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      // get userdetail from header
+      const user = userDetail(req);
+  
+      // exapress validate input
+      const errors = validationResult(req);
+      if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+  
+      // get input
+      const product : products = req.body;
+  
+      // venue profile input with typescript definition
+      const productInput : products = {
+        id: uuidv4(),            
+        venueId: user.sub,
+        productName: product.productName,
+        description: product.description,
+        price: product.price,
+        category: product.category,
+        images: product.images,
+        proteinType: product.proteinType,
+        isActive: product.isActive,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      }
+  
+      // dynamodb parameter
+      const paramsDB : AWS.DynamoDB.DocumentClient.PutItemInput = {
+        TableName: productsModel.TableName,
+        Item: productInput,
+        ConditionExpression: 'attribute_not_exists(venueId)'
+      }
+  
+      // save data to database
+      await ddb.put(paramsDB).promise();
+  
+      // return result
+      return res.status(200).json({
+        code: 200,
+        message: 'success',
+        data: paramsDB?.Item
+      });
+  
+    } catch (e) {
+      
+      /**
+       * Return error kalau expression data udh ada
+       */
+      if(e?.code == 'ConditionalCheckFailedException') {
+        next(new Error('Data Already Exist.!'));
+      }
+  
+      // return default error
+      next(e);
+    }
+  };
